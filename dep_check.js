51c07a7bf77c411fd691193c865618972b8a41a1
Athisruth <athisruthmsatheesh98@gmail.com>
2025-02-10 13:43:19 +0530

    "installed prompt-sync package for taking user input for file_to_check"

--- dep_check.js
+++ dep_check.js
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
+import promptSync from "prompt-sync";
 import {file_path_finder} from "./file_path_finder.js";
 import {finding_getimports} from "./finding_getimports.js"
 
+const prompt = promptSync();
 
 function depSearch(fileToCheck, importData, visited = new Set(), tupledDependencies = new Set()) {
     visited.add(fileToCheck);
@@ -39,7 +41,7 @@ const rawData = file_path_finder(folderPath)
 const importData = finding_getimports(rawData);  
 
 
-let fileToCheck = "utils.js";
+const fileToCheck = prompt("Enter the file name to check dependencies: ");
 
 
 if (fileToCheck) {
